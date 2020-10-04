4a021456023867e4d2d5561a8cb2611468ee9f6b
Andrew Long <andrewmlong@gmail.com>
2020-10-03 23:20:01 -0400

    some refactoring + CFs + BS

--- cayce/xbrl.py
+++ cayce/xbrl.py
@@ -20,6 +20,8 @@ class FinancialDocumentParser:
             contents = fin.read()
         self._soup = BeautifulSoup(contents, "lxml")
 
+        self._parse_relevant_contexts()
+
     def _parse_relevant_contexts(self):
         """
         Get a list of contexts that provide data on a date period
@@ -96,11 +98,16 @@ class FinancialDocumentParser:
         )
 
     def _get_attribute_values_df(
-        self, tag_labels: List[str], numeric_only: bool = True
+        self, attributes: List[str], numeric_only: bool = True
     ):
+        """
+        Get a normalized DataFrame of all attributes provided from the pre-loaded
+        BeautifulSoup object.  If numeric_only, skip any non-numeric attributes and
+        parse the values captured as floats
+        """
         rows = []
         processed_elements = set()
-        for tag_label in tag_labels:
+        for tag_label in attributes:
             tags = self._soup.find_all(name=re.compile(tag_label, re.IGNORECASE))
 
             for tag in tags:
@@ -156,8 +163,8 @@ class FinancialDocumentParser:
         company_specific_tags = [f"{self._ticker}:.*"]
         return self._get_attribute_values_df(company_specific_tags)
 
-    def get_dei_attributes_df(self) -> pd.DataFrame:
-        header_tag_labels = [
+    def parse_dei_attributes(self) -> pd.DataFrame:
+        attributes = [
             "dei:DocumentType",
             "dei:DocumentPeriodEndDate",
             "dei:EntityRegistrantName",
@@ -166,10 +173,10 @@ class FinancialDocumentParser:
             "dei:EntityCommonStockSharesOutstanding",
         ]
         # These attributes will be text
-        return self._get_attribute_values_df(header_tag_labels, False)
+        return self._get_attribute_values_df(attributes, False)
 
-    def get_income_statement_df(self) -> pd.DataFrame:
-        income_statement_attributes = [
+    def parse_income_statement(self) -> pd.DataFrame:
+        attributes = [
             "us-gaap:Revenues",
             "us-gaap:CostOfGoodsAndServicesSoldAmortization",
             "us-gaap:CostOfGoodsAndServicesSold",
@@ -199,5 +206,121 @@ class FinancialDocumentParser:
             "us-gaap:WeightedAverageNumberOfSharesOutstandingBasic",
             "us-gaap:WeightedAverageNumberOfDilutedSharesOutstanding",
         ]
-        income_statement_df = _get_attribute_values_df(income_statement_attributes)
+        income_statement_df = self._get_attribute_values_df(attributes)
         return income_statement_df
+
+    def parse_comprehensive_income(self) -> pd.DataFrame:
+        attributes = [
+            "us-gaap:OtherComprehensiveIncomeForeignCurrencyTransactionAndTranslationAdjustmentNetOfTaxPortionAttributableToParent",
+            "us-gaap:OtherComprehensiveIncomeLossForeignCurrencyTransactionAndTranslationReclassificationAdjustmentFromAOCIRealizedUponSaleOrLiquidationBeforeTax"
+            "us-gaap:OtherComprehensiveIncomeLossPensionAndOtherPostretirementBenefitPlansAdjustmentNetOfTax",
+            "us-gaap:OtherComprehensiveIncomeUnrealizedHoldingGainLossOnSecuritiesArisingDuringPeriodNetOfTax",
+            "us-gaap:OtherComprehensiveIncomeLossNetOfTax",
+            "us-gaap:ComprehensiveIncomeNetOfTax",
+        ]
+        comprehensive_income_df = self._get_attribute_values_df(attributes)
+        return comprehensive_income_df
+
+    def parse_balance_sheet(self) -> pd.DataFrame:
+        attributes = [
+            "us-gaap:AllowanceForDoubtfulAccountsReceivableCurrent",
+            "us-gaap:AvailableForSaleSecuritiesAmortizedCost",
+            "us-gaap:CashAndCashEquivalentsAtCarryingValue",
+            "us-gaap:AvailableForSaleSecuritiesCurrent",
+            "us-gaap:AccountsReceivableNetCurrent",
+            "us-gaap:PrepaidExpenseAndOtherAssetsCurrent",
+            "us-gaap:AssetsOfDisposalGroupIncludingDiscontinuedOperationCurrent",
+            "us-gaap:AssetsCurrent",
+            "us-gaap:MarketableSecuritiesNoncurrent",
+            "us-gaap:PropertyPlantAndEquipmentNet",
+            "us-gaap:Goodwill",
+            "us-gaap:IntangibleAssetsNetExcludingGoodwill",
+            "us-gaap:OperatingLeaseRightOfUseAsset",
+            "us-gaap:OtherAssetsNoncurrent",
+            "us-gaap:DisposalGroupIncludingDiscontinuedOperationOtherNoncurrentAssets",
+            "us-gaap:Assets",
+            "us-gaap:LongTermDebtAndCapitalLeaseObligationsCurrent",
+            "us-gaap:AccountsPayableCurrent",
+            "us-gaap:AccruedLiabilitiesCurrent",
+            "us-gaap:DeferredRevenueCurrent",
+            "us-gaap:LiabilitiesOfDisposalGroupIncludingDiscontinuedOperationCurrent",
+            "us-gaap:LiabilitiesCurrent",
+            "us-gaap:LongTermDebtAndCapitalLeaseObligations",
+            "us-gaap:DeferredRevenueNoncurrent",
+            "us-gaap:DeferredIncomeTaxLiabilities",
+            "us-gaap:OperatingLeaseLiabilityNoncurrent",
+            "us-gaap:OtherLiabilitiesNoncurrent",
+            "us-gaap:LiabilitiesOfDisposalGroupIncludingDiscontinuedOperationNoncurrent",
+            "us-gaap:Liabilities",
+            "us-gaap:CommitmentsAndContingencies",
+            "us-gaap:CommonStockParOrStatedValuePerShare",
+            "us-gaap:CommonStockSharesAuthorized",
+            "us-gaap:CommonStockSharesIssued",
+            "us-gaap:CommonStockSharesOutstanding",
+            "us-gaap:TreasuryStockShares",
+            "us-gaap:CommonStockValue",
+            "us-gaap:AdditionalPaidInCapital",
+            "us-gaap:TreasuryStockValue",
+            "us-gaap:AccumulatedOtherComprehensiveIncomeLossNetOfTax",
+            "us-gaap:RetainedEarningsAccumulatedDeficit",
+            "us-gaap:StockholdersEquity",
+            "us-gaap:MinorityInterest",
+            "us-gaap:MembersEquity",
+            "us-gaap:LiabilitiesAndStockholdersEquity",
+        ]
+        balance_sheet_df = self._get_attribute_values_df(attributes)
+        return balance_sheet_df
+
+    def parse_cash_flows(self):
+        attributes = [
+            "us-gaap:IncomeLossFromContinuingOperations",
+            "us-gaap:Depreciation",
+            "us-gaap:AmortizationOfIntangibleAssets",
+            "us-gaap:ShareBasedCompensation",
+            "us-gaap:AmortizationOfFinancingCosts",
+            "us-gaap:DeferredIncomeTaxExpenseBenefit",
+            "us-gaap:GainsLossesOnExtinguishmentOfDebt",
+            "us-gaap:OtherNoncashIncomeExpense",
+            "us-gaap:IncreaseDecreaseInAccountsReceivable",
+            "us-gaap:IncreaseDecreaseInPrepaidDeferredExpenseAndOtherAssets",
+            "us-gaap:IncreaseDecreaseInAccountsPayableTrade",
+            "us-gaap:IncreaseDecreaseInAccruedLiabilities",
+            "us-gaap:IncreaseDecreaseInDeferredRevenue",
+            "us-gaap:NetCashProvidedByUsedInContinuingOperations",
+            "us-gaap:CashProvidedByUsedInOperatingActivitiesDiscontinuedOperations",
+            "us-gaap:NetCashProvidedByUsedInOperatingActivities",
+            "us-gaap:PaymentsToAcquirePropertyPlantAndEquipment",
+            "us-gaap:ProceedsFromDivestitureOfBusinesses",
+            "us-gaap:PaymentsToAcquireInvestments",
+            "us-gaap:ProceedsFromSaleMaturityAndCollectionsOfInvestments",
+            "us-gaap:PaymentsForProceedsFromOtherInvestingActivities",
+            "us-gaap:NetCashProvidedByUsedInInvestingActivities",
+            "us-gaap:RepaymentsOfLongTermDebtAndCapitalSecurities",
+            "us-gaap:ProceedsFromEquityMethodInvestmentDividendsOrDistributionsReturnOfCapital",
+            "us-gaap:PaymentsForRepurchaseOfCommonStock",
+            "us-gaap:ProceedsFromIssuanceOfSharesUnderIncentiveAndShareBasedCompensationPlansIncludingStockOptions",
+            "us-gaap:ProceedsFromLinesOfCredit",
+            "us-gaap:RepaymentsOfUnsecuredDebt",
+            "us-gaap:PaymentsRelatedToTaxWithholdingForShareBasedCompensation",
+            "us-gaap:ProceedsFromPaymentsForOtherFinancingActivities",
+            "us-gaap:NetCashProvidedByUsedInFinancingActivities",
+            "us-gaap:EffectOfExchangeRateOnCashAndCashEquivalents",
+            "us-gaap:CashAndCashEquivalentsPeriodIncreaseDecrease",
+            "us-gaap:CashAndCashEquivalentsAtCarryingValue",
+        ]
+        cash_flow_statement_df = self._get_attribute_values_df(attributes)
+        return cash_flow_statement_df
+
+    def parse_all(self) -> pd.DataFrame:
+        """
+        Parse as much data out of this document as possible, and return all of it
+        in one huge normalized DataFrame
+        """
+        dfs = [
+            self.parse_dei_attributes(),
+            self.parse_income_statement(),
+            self.parse_comprehensive_income(),
+            self.parse_balance_sheet(),
+            self.parse_cash_flows(),
+        ]
+        return pd.concat(dfs, ignore_index=True)
