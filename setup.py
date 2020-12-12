8a58f510d53d40a46e4aaacc1fbf478e564ddda0
Andrew Long <andrewmlong@gmail.com>
2020-12-12 13:29:45 -0500

    Finish the setup script...

--- setup.py
+++ setup.py
@@ -5,3 +5,21 @@ requirements = [
     "pandas >= 1.0.5",
     "wget >= 3.2",
 ]
+
+st.setup(
+    name="cayce",
+    version="0.1.0",
+    author="Andrew Long",
+    author_email="andrewmlong@hotmail.com",
+    description="Tools to search and download filing data from SEC EDGAR",
+    url="https://github.com/along1x/cayce",
+    packages=st.find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero GPL",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
